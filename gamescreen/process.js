cd98b5f83ee4e0fd9119f2948b3233943bf522f7
Leo <kychancv@connect.ust.hk>
2023-04-16 01:13:04 +0800

    Question

--- gamescreen/process.js
+++ gamescreen/process.js
@@ -1,27 +1,12 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-const door = Door(context, 320, 144);
-const player = Player(context, 304, 152);
-let smokes = [];
-
-let hitCount = 0;
-let hitLeft = 200;
+const player = Player(context, 176, 192);
 
 let submitted = false;
 
 function setSubmitted(val) {
   submitted = val;
-}
-
-function setHitCount(count) {
-  resetHit();
-  hitCount = count;
-}
-
-function resetHit() {
-  hitCount = 0;
-  hitLeft = 200;
 }
 
 function sendSuccess() {
@@ -45,56 +30,17 @@ function sendFail() {
 function doFrame(now) {
     if (submitted) {
       document.getElementById("submit").disabled = true;
-      door.repair();
-
-      if (hitLeft == 0) {
-        setTimeout(sendSuccess, 1000);
-      } else if (hitCount == 0) {
-        if (hitLeft > 0) {
-          setTimeout(sendFail, 1000);
-        }
-      }
-    }
-
-    if (hitCount > 0 && hitLeft > 0) {
-      hitCount--;
-      hitLeft--;
-
-      let randomX = Math.floor(Math.random()*48) + 296;
-      smokes.push(Smoke(context, randomX, 144));
-      player.setXY(randomX, 152)
-    }
-
-    if (smokes.length > 5) {
-      smokes.shift();
+      setTimeout(sendSuccess, 1000);
     }
 
     /* Update the sprites */
-    if (hitLeft <= 0) {
-      door.destroy();
-    }
-
-    door.update(now);
-    for (let i = 0; i < smokes.length; i++) {
-      smokes[i].update(now);
-    }
     player.update(now);
 
     /* Clear the screen */
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     /* Draw the sprites */
-    door.draw();
-    for (let i = 0; i < smokes.length; i++) {
-      smokes[i].draw();
-    }
     player.draw();
-
-    /* How many hit left */
-    context.font = "30px Consolas";
-    context.fillStyle = "red";
-    context.textAlign = "center";
-    context.fillText(hitLeft, 320, 120);
 
     /* Process the next frame */
     requestAnimationFrame(doFrame);
--- ide/ide.js
+++ ide/ide.js
@@ -9,39 +9,91 @@ editor.setOptions({
 
 
 let defaultCode = 
-`// You are given the following function:
-// hit() -- Hit the door once
+`// Finish the function move()
+// (x, y) is the position you are currently at.
+// x increases from left to right of the map.
+// y increases from top to bottom of the map.
+// direction can be "Right", "Up" or "Down".
+// step indicates how many steps you move in a direction.
 
-for () {
+function move(x, y, direction, step) {
+    let newX = x, newY = y;
+    
+    if (direction == "Down") {
+        newY += step;
+    }
+    
+    // TODO
+    
+    return [newX, newY];
+}
 
-}`
+// The function will be called:
+// move(x, y, "Right", 3);
+// move(x, y, "Down", 2);
+// move(x, y, "Right", 2);
+// move(x, y, "Up", 1);
+// move(x, y, "Right", 4);`
 
-editor.session.setValue(defaultCode);
+let answer =
+`
+function move(x, y, direction, step) {
+    let newX = x, newY = y;
+    
+    if (direction == "Down") {
+        newY += step;
+    } else if (direction == "Up") {
+        newY -= step;
+    } else if (direction == "Right") {
+        newX += step;
+    }
+    
+    return [newX, newY];
+}
+`
+
+editor.session.setValue(answer);
 
 let submitButton = document.getElementById("submit");
 let resetButton = document.getElementById("reset")
 
 let skeletonBefore = 
 `
-let count = 0;
-
-function hit() {
-    count += 1;
-}
-
+let playerX = 5.5, playerY = 6;
+let coordinates = [];
 `
 
 let skeletonAfter = 
 `
+let tempCoor = move(playerX, playerY, "Right", 3);
+playerX = tempCoor[0], playerY = tempCoor[1];
+coordinates.push([playerX, playerY]);
 
-count.valueOf();`
+tempCoor = move(playerX, playerY, "Down", 2);
+playerX = tempCoor[0], playerY = tempCoor[1];
+coordinates.push([playerX, playerY]);
+
+tempCoor = move(playerX, playerY, "Right", 2);
+playerX = tempCoor[0], playerY = tempCoor[1];
+coordinates.push([playerX, playerY]);
+
+tempCoor = move(playerX, playerY, "Up", 1);
+playerX = tempCoor[0], playerY = tempCoor[1];
+coordinates.push([playerX, playerY]);
+
+tempCoor = move(playerX, playerY, "Right", 4);
+playerX = tempCoor[0], playerY = tempCoor[1];
+coordinates.push([playerX, playerY]);
+
+coordinates;
+`
 
 submitButton.addEventListener('click', function() {
     let code = editor.getValue();
     try {
         let result = eval(skeletonBefore + code + skeletonAfter);
+        console.log(result);
         setSubmitted(true);
-        setHitCount(result);
     } catch (err) {
         console.error(err);
         document.getElementById("alertSuccess").style.display = "none";
@@ -53,5 +105,7 @@ submitButton.addEventListener('click', function() {
 resetButton.addEventListener('click', function() {
     try {
         editor.session.setValue(defaultCode);
-    } catch (error) {}
+    } catch (error) {
+        console.log(error);
+    }
 }, false);
